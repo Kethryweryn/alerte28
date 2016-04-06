622c2be1904da97968d5a75af03eceef8994e648
Xargos <kethryweryn@gmail.com>
2016-04-06 22:01:08 +0200

    Correction { en trop après un if

--- dbAccess.php
+++ dbAccess.php
@@ -1,13 +1,11 @@
 <?php
 require_once "dbConfig.php";
-class dbAccess
-{
+class dbAccess {
 	private $db;
 	private $host;
 	private $login;
 	private $password;
-	public function __construct()
-	{
+	public function __construct() {
 		$this->db = DB_NAME;
 		$this->host = DB_HOST;
 		$this->login = DB_LOGIN;
@@ -16,44 +14,43 @@ class dbAccess
 	
 	/**
 	 * Connextion à la BDD
-	 * @param -
+	 * 
+	 * @param
+	 *        	-
 	 * @return mysqli Object
 	 */
-	private function connect()
-	{
-		$mysqli = new mysqli($this->host, $this->login, $this->password, $this->db);
-		if ($mysqli->connect_error) {
-    		die('Erreur de connexion (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
+	private function connect() {
+		$mysqli = new mysqli ( $this->host, $this->login, $this->password, $this->db );
+		if ($mysqli->connect_error)
+			die ( 'Erreur de connexion (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error );
 		return $mysqli;
 	}
 	
-	/** 
+	/**
 	 * Passe une requête sur la base de données
-	 * @param $rq string
+	 * 
+	 * @param $rq string        	
 	 * @return $ret : mysqli object
 	 */
-	public function query($rq)
-	{
-		$mysqli = $this->connect();
-		$ret = $mysqli->query($rq);
-		$mysqli->close();
+	public function query($rq) {
+		$mysqli = $this->connect ();
+		$ret = $mysqli->query ( $rq );
+		$mysqli->close ();
 		return $ret;
 	}
 	
 	/**
 	 * Renvoie un tableau d'objets
-	 * @param $rq string
+	 * 
+	 * @param $rq string        	
 	 * @return array[object]
 	 */
-	public function select($rq)
-	{
-		$ret = $this->query($rq);
-		$array_ret = array();
+	public function select($rq) {
+		$ret = $this->query ( $rq );
+		$array_ret = array ();
 		
-		while($obj = $ret->fetch_object())
-		{
-			$array_ret[] = $obj;
+		while ( $obj = $ret->fetch_object () ) {
+			$array_ret [] = $obj;
 		}
 		return $array_ret;
 	}
