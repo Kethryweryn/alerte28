05af050cbfa8d2ecec2473dc3ebad8e96aa87a86
Kethryweryn <kethryweryn@gmail.com>
2016-05-14 17:55:16 +0200

    Modifications de base

--- daemon.php
+++ daemon.php
@@ -11,15 +11,27 @@ $event_manager = new eventManager($db_access);
 $event_solver = new eventSolver($db_access);
 
 while (1) {
-	// Lecture en bdd voir si de nouveaux changements sont là
-	// Puis on gère ces événements
-	foreach ($event_manager->getNewUserActions() as $user_action) {
-		try {
-			$event_solver->solve($user_action);
-		} catch (Exception $e) {
-			$event_solver->markAsError($user_action);
+	try
+	{
+		// On commence une transaction
+		$dbAccess->begin();
+
+		// Lecture en bdd voir si de nouveaux changements sont là
+		// Puis on gère ces événements
+		foreach ($event_manager->getNewUserActions() as $user_action) {
+			try {
+				$event_solver->solve($user_action);
+			} catch (Exception $e) {
+				$event_solver->markAsError($user_action);
+			}
 		}
-		$event_manager->deleteUserAction($user_action);
+
+		// On truncate la table des user actions
+
+		// On commit la transaction
+		$dbAccess->commit();
+	} catch (Exception $e) {
+		$dbAccess->rollback();
 	}
 
 	// Gestion des événements à durée limitée
