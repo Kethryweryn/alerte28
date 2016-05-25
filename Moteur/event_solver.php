da071a88f4c69e41276eb04c680151cf268dbc8e
Xargos <kethryweryn@gmail.com>
2016-05-25 23:03:07 +0200

    Dev continued

--- Moteur/event_solver.php
+++ Moteur/event_solver.php
@@ -15,5 +15,17 @@ class EventSolver {
 		// Voici les impacts possibles :
 		// - modifier les compteurs (fonction d'event_manager qui modifie la table a28_counters)
 		// - terminer l'event, ce qui change le statut de l'event et active les événements suivants dans l'arbre
+		
+		// On récupère la liste des action_event en fonction d'action_id et event_id
+		// (Note : action_id peut être NULL si c'est la résolution par défaut)
+		
+		$stmt = $db->prepare("SELECT * from `a28_action_event` WHERE event_id = ? AND action_id = ? ;");
+		$stmt->bind_param($user_action->event_id, $user_action->action_id);
+		$stmt->bind_result(); // TODO
+		
+		while($stmt->fetch())
+		{
+			
+		}
 	}
 }
