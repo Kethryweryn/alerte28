1cb00fef3ba57455cc9346c901c53278a67a9554
Xargos <kethryweryn@gmail.com>
2016-06-09 22:31:09 +0200

    Modifications pour la gestion des actes

---
+++ SQL/20160609-updates.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `a28_act` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `start_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `a28_event`
+  DROP `predecessor`,
+  DROP `successor`;
+  
+  ALTER TABLE `a28_event` ADD `start_on` DATETIME NULL , ADD `act_id` INT NOT NULL , ADD `start_with_act` BOOLEAN NOT NULL DEFAULT FALSE , ADD INDEX (`act_id`) ;
--- daemon.php
+++ daemon.php
@@ -2,48 +2,47 @@
 require_once 'dbAccess.php';
 
 // Connexion bdd
-$db_access = new dbAccess();
+$db_access = new dbAccess ();
 
 // On instancie une classe qui est en charge de gérer les events
-$event_manager = new EventManager($db_access);
+$event_manager = new EventManager ( $db_access );
 
 // On instancie une classe qui est en charge de résoudre les events
-$event_solver = new EventSolver($db_access);
+$event_solver = new EventSolver ( $db_access );
 
-while (1) {
-	try
-	{
+while ( 1 ) {
+	try {
 		// On commence une transaction
-		$dbAccess->begin();
+		$dbAccess->begin ();
 
 		// Lecture en bdd voir si de nouveaux changements sont là
 		// Puis on gère ces événements
-		foreach ($event_manager->getNewUserActions() as $user_action) {
+		foreach ( $event_manager->getNewUserActions () as $user_action ) {
 			try {
-				$event_solver->solve($user_action);
-			} catch (Exception $e) {
-				$event_solver->markAsError($user_action);
+				$event_solver->solve ( $user_action );
+			} catch ( Exception $e ) {
+				$event_solver->markAsError ( $user_action, $e );
 			}
 		}
 
 		// On truncate la table des user actions
-		$dbAccess->query("TRUNCATE TABLE a28_user_action ;");
+		$dbAccess->query ( "TRUNCATE TABLE a28_user_action ;" );
 
 		// On commit la transaction
-		$dbAccess->commit();
-	} catch (Exception $e) {
-		$dbAccess->rollback();
+		$dbAccess->commit ();
+	} catch ( Exception $e ) {
+		$dbAccess->rollback ();
 	}
 
 	// Gestion des événements à durée limitée
-	
+
 	// Il faut une gestion en actes donc il faut que chaque événement soit lié à un acte
 	// Il faut pouvoir déclencher le début et la fin d'un acte
-	
+
 	// On récupère la liste des événements en cours
 	// On fait la différence entre le start date de l'événement et sa durée
 	// Si le temps est dépassé on solve l'événement avec la résolution par défaut
 	// La résolution par défaut se fait en prenant les actions typées "default" (booléen) dans la table action_event
 
-	sleep(1);
+	sleep ( 1 );
 }
