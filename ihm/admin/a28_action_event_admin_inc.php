99e1ff4204db97612dfb4aecabc3c42fa6831245
totoskul <totoskul@192.168.0.31>
2016-06-15 22:46:21 +0200

    mise en place du formulaire pour rpendre en compte les résultats des actions sur les events. CODE NON TESTE sur la récup du POST !!

--- ihm/admin/a28_action_event_admin_inc.php
+++ ihm/admin/a28_action_event_admin_inc.php
@@ -5,6 +5,28 @@ if('a28_action_event_admin_inc.php'==$_SERVER['PHP_SELF']){
 	header('location:index.php?e=6');
 }
 $db = new dbAccess();
+if(isset($_POST))
+{
+	// nettoyage de toutes les données d'impact
+	$rq = "delete from a28_action_event where event_id = ".$_GET['eid'];
+	$db->query($rq);
+	foreach($_POST as $val => $impact)
+	{
+		$array = explode("_", $val);
+		if(!empty($impact))
+			if($array[0] == "tex")
+			{
+				// on peut faire l'insert
+				$rq = "insert into a28_action_event (action_id, event_id, impact, counter_id, end_event) VALUES (".
+						$array[2].", ".
+						$_GET['eid'].", ".
+						$impact.", ".
+						$_POST['sel_'.$array[1]."_".$array[2]].", false)";
+				
+				$db->query($rq);
+			}
+	}
+}
 
 $rq = "select a.service_id, b.nom as service_name, a.name as action_name, a.id as action_id from a28_action a
 		join a28_service b on b.id = a.service_id
@@ -12,21 +34,6 @@ $rq = "select a.service_id, b.nom as service_name, a.name as action_name, a.id a
 		";
 
 $result = $db->select($rq);
-?>
-<script type="text/javascript">
-<?php 
-foreach($result as $action)
-{
-	// TODO ajout dynamique de content suite au click dans le fieldset.
-	echo '
-		$( "#butt_'.$action->action_id.'" ).click(function(){
-    $("div_'.$action->action_id.'").append("<b>Appended text</b>");
-}); ';
-}
-?>
-
-</script>
-<?php 
 
 function fillSelect($name)
 {
@@ -45,7 +52,9 @@ function fillSelect($name)
 	$html_select .= "</select>";
 	return $html_select;
 }
-
+?>
+<form method=POST action="gestion.php?page=a28_action_event_admin?eid=<?php echo $_GET['eid']; ?>"> 
+<?php 
 
 $service = 0;
 foreach( $result as $action)
@@ -61,11 +70,20 @@ foreach( $result as $action)
 		$service = $action->service_id;
 		
 	}
-	echo "<fieldset><legend>".$action->action_name."</legend><div id=div_".$action->action_id.">".fillSelect('sel_a_'.$action->action_id)."
-			<input name=tex_a_".$action->action_id." type=text></div><button id=butt_".$action->action_id." >+</button></fieldset>";
+	// TODO il faut mettre la valeur par défaut... 
+	echo "<fieldset><legend>".$action->action_name."</legend>
+			<div id=div_".$action->action_id.">".
+				fillSelect('sel_a_'.$action->action_id)."
+				<input name=tex_a_".$action->action_id." type=text></div><div>".
+				fillSelect('sel_b_'.$action->action_id)."
+				<input name=tex_b_".$action->action_id." type=text></div><div>".
+				fillSelect('sel_c_'.$action->action_id)."
+				<input name=tex_c_".$action->action_id." type=text>
+			</div>
+			</fieldset>";
 	
 }
 echo "</fieldset>";
 ?>
-
- 
+<input type=submit value="Sauvegarder">
+</form>
