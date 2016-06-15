18debdd6968a87bbd05627f97eadb8a65c42a17a
totoskul <totoskul@192.168.0.31>
2016-06-15 21:05:25 +0200

    suppression de la mention du nom de la base dans la requête

--- SQL/20160615-updates.sql
+++ SQL/20160615-updates.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `a28_service_history` ADD `is_error` BOOLEAN NOT NULL DEFAULT FALSE , ADD `error_message` TEXT NULL ;
 ALTER TABLE `a28_user_action` ADD `service_id` INT NOT NULL , ADD INDEX (`service_id`) ;
 
-RENAME TABLE `alerte28`.`a28_service_history` TO `alerte28`.`a28_action_history`;
+RENAME TABLE `a28_service_history` TO `a28_action_history`;
 ALTER TABLE `a28_action_history` CHANGE `service_id` `service_id` INT(11) NULL, CHANGE `user_id` `user_id` INT(11) NULL;
