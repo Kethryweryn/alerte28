fd81d8f5318148488c58dec000a61e1a952dc6e0
totoskul <totoskul@192.168.0.31>
2016-09-26 21:31:59 +0200

    Mise en conformité de l'apparence de l'Interface vs les fils de communication CIUB

--- ihm/admin/gestion.php
+++ ihm/admin/gestion.php
@@ -21,21 +21,82 @@ echo('<?xml version="1.0"?>');
 ?>
 
 <!DOCTYPE html>
-<html class=" js mozilla">
-	<head>
-		<title>Console</title>
-		<meta name="Robots" content="noindex,nofollow">
-		<meta http-equiv="X-UA-Compatible" content="IE=EDGE">
-		<meta name="viewport" content="" id="viewport">
+<html>
+<head>
+  <title>Dashboard Starter UI, by Keen IO</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+  <link rel="stylesheet" type="text/css" href="../../template/assets/lib/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="../../template/assets/css/keen-dashboards.css" />
+<style>
+  
+  blockquote.twitter-tweet {
+  display: inline-block;
+  font-family: "Helvetica Neue", Roboto, "Segoe UI", Calibri, sans-serif;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 16px;
+  border-color: #eee #ddd #bbb;
+  border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
+  margin: 10px 5px;
+  padding: 0 16px 16px 16px;
+  max-width: 468px;
+}
+ 
+blockquote.twitter-tweet p {
+  font-size: 16px;
+  font-weight: normal;
+  line-height: 20px;
+}
+ 
+blockquote.twitter-tweet a {
+  color: inherit;
+  font-weight: normal;
+  text-decoration: none;
+  outline: 0 none;
+}
+ 
+blockquote.twitter-tweet a:hover,
+blockquote.twitter-tweet a:focus {
+  text-decoration: underline;
+}
+  </style>
 		<link rel="stylesheet" type="text/css" href="../login_fichiers/styles.css">
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    
-	</head>
-
-	<body>
-		<div id="content-form">
+ 
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    </head>
+	<body class="application">
+	  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="../">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="navbar-brand" href="./">Suivi CIUB</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-left">
+          <li><a href="../../Google_charts/index.html">Home</a></li>
+          <li><a href="../../Google_charts/twitter.html">Twitter</a></li>
+          <li><a href="index.php">L'interface</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>		
+  		<div id="content-form">
 			<div class="box-inner">
 				<div>
 					<a href="gestion.php">
