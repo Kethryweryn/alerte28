3dbd2f9969967bb73124a48a8931675980ea474d
Xargos <kethryweryn@gmail.com>
2016-10-12 21:57:05 +0200

    correction accès variable interne db

--- Moteur/event_solver.php
+++ Moteur/event_solver.php
@@ -4,10 +4,10 @@ class EventSolver {
 	/**
 	 * Constructeur
 	 *
-	 * @param unknown $db_access
+	 * @param unknown $this->db_access
 	 */
 	public function __construct($db_access) {
-		$this->$db = $db_access;
+		$this->db = $db_access;
 	}
 	/**
 	 * Résolution d'action ou d'événement
@@ -33,7 +33,7 @@ class EventSolver {
 			$rq .= "AND action_id IS NULL";
 		$rq .= " ;";
 
-		$stmt = $db->prepare ( $rq );
+		$stmt = $this->db->prepare ( $rq );
 		if ($user_action->action_id)
 			$stmt->bind_param ( "ii", $user_action->event_id, $user_action->action_id );
 		else
@@ -45,26 +45,26 @@ class EventSolver {
 			// On résout ce que fait l'action
 			if ($counter_id) {
 				$rq_counter = "INSERT INTO a28_counters(counter_ref_id, event_on, value) VALUES(?, NOW(), ?) ;";
-				$stmt_counter = $db->prepare ( $rq_counter );
+				$stmt_counter = $this->db->prepare ( $rq_counter );
 				$stmt_counter->bind_param ( "id", $counter_id, $impact );
 				$stmt_counter->execute ();
 			}
 			if ($end_event) {
 				$rq_event = "UPDATE a28_event SET enabled = FALSE WHERE id = ? ;";
-				$stmt_event = $db->prepare ( $rq_event );
+				$stmt_event = $this->db->prepare ( $rq_event );
 				$stmt_event->bind_param ( "i", $user_action->event_id );
 				$stmt_event->execute ();
 			}
 			if ($next_event_id) {
 				$rq_next_event = "UPDATE a28_event SET enabled = TRUE, start_on = NOW() WHERE id = ? ;";
-				$stmt_next_event = $db->prepare ( $rq_next_event );
+				$stmt_next_event = $this->db->prepare ( $rq_next_event );
 				$stmt_next_event->bind_param ( "i", $next_event_id );
 				$stmt_next_event->execute ();
 			}
 		}
 
 		// On sauvegarde l'historique
-		$stmt_history = $db->prepare ( "INSERT INTO a28_action_history(service_id, user_id, action_id, action_on) VALUES (?, ?, ?, NOW())" );
+		$stmt_history = $this->db->prepare ( "INSERT INTO a28_action_history(service_id, user_id, action_id, action_on) VALUES (?, ?, ?, NOW())" );
 		$stmt_history->bind_param ( "iii", $user_action->service_id, $user_action->user_id, $user_action->action_id );
 		$stmt_history->execute ();
 	}
@@ -75,7 +75,7 @@ class EventSolver {
 	 * @param UserAction $user_action
 	 */
 	public function markAsError(UserAction $user_action, Exception $e) {
-		$stmt_history = $db->prepare ( "INSERT INTO a28_action_history(service_id, user_id, action_id, action_on, is_error, error_message) VALUES (?, ?, ?, NOW(), 1, ?)" );
+		$stmt_history = $this->db->prepare ( "INSERT INTO a28_action_history(service_id, user_id, action_id, action_on, is_error, error_message) VALUES (?, ?, ?, NOW(), 1, ?)" );
 		$stmt_history->bind_param ( "iiis", $user_action->service_id, $user_action->user_id, $user_action->action_id, $e->getMessage() );
 		$stmt_history->execute ();
 	}
