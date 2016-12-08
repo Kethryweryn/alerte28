326b58ffb45357238d3897adcd796f54329c6f8e
Kethryweryn <kethryweryn@gmail.com>
2016-12-08 22:45:31 +0100

    gestion des actes

--- game_engine.php
+++ game_engine.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'dbAccess.php';
 
-$params = getopt("", array("start", "next", "stop"));
+$params = getopt("", array("start", "act:", "stop"));
 
 // Connexion bdd
 $db_access = new dbAccess ();
@@ -14,10 +14,20 @@ if(in_array($params, "start"))
 		$rq_params = "INSERT INTO a28_params(event_name, event_value) VALUES("start_date", NOW())";
 		$stmt_params = $db_access->prepare ( $rq_params );
 		$stmt_params->execute ();
-		$stmt_params->close();
-		// On commit la transaction
+		$stmt_params->close();		
 		$db_access->commit ();
+		
 	} catch ( Exception $e ) {
 		$db_access->rollback ();
 	}
+}
+
+if(in_array($params, "act"))
+{
+	$act_nb = $params["act"];
+	
+	if (!is_int($act_nb))
+		exit(-1);
+	
+	// On démarre l'acte demandé
 }
